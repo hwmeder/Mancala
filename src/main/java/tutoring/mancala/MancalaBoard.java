fc73162847196126a1d8a8910ae552ee607a9896
hwmeder <hmeder@overstock.com>
2019-03-15 19:23:36 -0600

    Accept configuration from command line.
    Validate configuration.
    Provide Help

--- src/main/java/tutoring/mancala/MancalaBoard.java
+++ src/main/java/tutoring/mancala/MancalaBoard.java
@@ -1,19 +1,23 @@
 package tutoring.mancala;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
 public class MancalaBoard {
 
+	private final List<Player> players = new ArrayList<>();
+	private final Player firstPlayer;
 
-	private Set<Player> players = new HashSet<>();
-
-	public MancalaBoard(String[] playerNames, int pitsPerPlayer, int stonesPerPit, Scanner kbd) {
+	public MancalaBoard(List<String> playerNames, int pitsPerPlayer, int stonesPerPit, Scanner kbd) {
 		super();
-		Iterator<String> playerNameItr = Arrays.asList(playerNames).iterator();
+		Collections.reverse(playerNames);
+		Iterator<String> playerNameItr = playerNames.iterator();
 
 		Player first = new Player(playerNameItr.next(), pitsPerPlayer, kbd, stonesPerPit);
 		players.add(first);
@@ -25,12 +29,13 @@ public class MancalaBoard {
 		}
 
 		first.withOpponent(last);
+		firstPlayer = last;
 	}
 
 	public void play() {
 		System.out.println("Here's the board; your pits are on the left, followed by your mancala.");
-		
-		Player player = players.iterator().next();
+
+		Player player = firstPlayer;
 		while (player.move()) {
 			player = player.getOpponent();
 		}
@@ -47,7 +52,7 @@ public class MancalaBoard {
 
 	private void displayResults(Set<Player> winners) {
 		System.out.println("Winners:");
-		for(Player winner : winners) {
+		for (Player winner : winners) {
 			winner.displayBoard();
 		}
 	}
--- src/main/java/tutoring/mancala/PlayMancala.java
+++ src/main/java/tutoring/mancala/PlayMancala.java
@@ -1,8 +1,17 @@
 package tutoring.mancala;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class PlayMancala {
+
+	private static final List<String> DEFAULT_playerNames = Arrays.asList(new String[] { "Bill", "Jill", "Carl" });
+	private static final int DEFAULT_stonesPerPit = 3;
+	private static final int DEFAULT_pitsPerPlayer = 4;
 
 	/**
 	 *
@@ -13,30 +22,81 @@ public class PlayMancala {
 	 */
 
 	public static void main(String[] args) {
-		Scanner kbd = new Scanner(System.in);
-		String[] playerNames = new String[] {"Bill", "Jill", "Carl"};
-		int pitsPerPlayer = 4;
-		int stonesPerPit = 3;
+		Iterator<String> argsItr = Arrays.asList(args).iterator();
 
-		try {
-			if (playerNames.length < 2) {
-				System.out.println("Mancala requires at least two players!");
+		int pitsPerPlayer;
+		if (argsItr.hasNext()) {
+			String arg = argsItr.next();
+			try {
+				pitsPerPlayer = Integer.parseInt(arg);
+			} catch (Exception e) {
+				System.out.println(arg + " is not a valid number of pits per player!\n" + e.getMessage());
+				displayHelp();
 				return;
 			}
+		} else {
+			pitsPerPlayer = DEFAULT_pitsPerPlayer;
+		}
+
+		int stonesPerPit;
+		if (argsItr.hasNext()) {
+			String arg = argsItr.next();
+			try {
+				stonesPerPit = Integer.parseInt(arg);
+			} catch (Exception e) {
+				System.out.println(arg + " is not a valid number of stones per pit!\n" + e.getMessage());
+				displayHelp();
+				return;
+			}
+		} else {
+			stonesPerPit = DEFAULT_stonesPerPit;
+		}
+
+		List<String> playerNames;
+		if (argsItr.hasNext()) {
+			playerNames = new ArrayList<>();
+			while (argsItr.hasNext()) {
+				playerNames.add(argsItr.next());
+			}
+		} else {
+			playerNames = DEFAULT_playerNames;
+		}
+
+		Scanner kbd = new Scanner(System.in);
+		try {
 			if (pitsPerPlayer < 1) {
 				System.out.println("Mancala requires at least one pit per player!");
+				displayHelp();
 				return;
 			}
 			if (stonesPerPit < 1) {
 				System.out.println("Mancala requires at least one stone per pit!");
+				displayHelp();
 				return;
 			}
+			if (playerNames.size() < 2) {
+				System.out.println("Mancala requires at least two players!");
+				displayHelp();
+				return;
+			}
+
 			MancalaBoard board = new MancalaBoard(playerNames, pitsPerPlayer, stonesPerPit, kbd);
 			board.play();
 		} finally {
 			kbd.close();
-			System.out.println("Thanks for playing.");
+			System.out.println("Thanks for playing Mancala.");
 		}
 
+	}
+
+	private static void displayHelp() {
+		System.out.println(PlayMancala.class.getSimpleName() +
+				" <number of pits per player> <number of stones per pit> <player names>\n" +
+				"Missing arguments will be defaulted.\n" +
+				"Defaults: " + DEFAULT_pitsPerPlayer + " " + DEFAULT_stonesPerPit + " " +
+				DEFAULT_playerNames.stream().collect(Collectors.joining(" ")) + "\n" +
+				"number of pits per player must be at least one\n" +
+				"number of stones per pit must be at least one\n" +
+				"number of player names must be at least two");
 	}
 }
